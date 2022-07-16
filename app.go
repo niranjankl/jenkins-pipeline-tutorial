d360ddf33c4c8356d4da12fa5592fdbadd989fe0
niranjankl <niranjan.kl@gmail.com>
2022-07-16 21:09:43 +0530

    Update app.go

--- app.go
+++ app.go
@@ -14,7 +14,7 @@ const (
 func main() {
 	h, _ := os.Hostname()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>It works!</h1>\n<h1>Hostname: %s</h1>\n<h1>Version: %s</h1>", h, version)
+		fmt.Fprintf(w, "<h1>New code works!</h1>\n<h1>Hostname: %s</h1>\n<h1>Version: %s</h1>", h, version)
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
